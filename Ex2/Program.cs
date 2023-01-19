da3fe0e42b01ef245a7a87b9e7c45d98a070a2c7
yulia <yuliagorbunova@yahoo.com>
2023-01-19 21:03:10 +0500

    commit2

---
+++ Ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя");
+string username = Console.ReadLine();
+if (username == "Маша")
+{ 
+    Console.WriteLine("Ура!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
